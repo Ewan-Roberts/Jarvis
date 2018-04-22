9b291e0b182fe8d48c0c8b66e3baa1fc3d07c11d
Ewan <jojothegreat694@hotmail.com>
2018-04-22 21:23:39 +0100

    Update boardSetUp.js

--- modules/boardSetUp.js
+++ modules/boardSetUp.js
@@ -1,23 +1,23 @@
 "use strict";
 
 const 	five 	= require("johnny-five"),
-		board 	= new five.Board(),
-		event 	= require("./event"),
-		storage = require("node-persist");
+      	board 	= new five.Board(),
+	event 	= require("./event"),
+	storage = require("node-persist");
 
 //Set up the modules on the ardino board and sets up events.on
 board.on("ready", () => {
-
-    console.log("board ready")
-		    //Bedroom modules 
-    const 	bedroomLightLeft 	= require("./bedroom/bedroomLeft.js"),
-    		bedroomLightRight 	= require("./bedroom/bedroomRight.js"),
-    		bedroomMotion 		= require("./bedroom/bedroomMotion.js"),
+	
+	console.log("board ready")
+	
+	//Bedroom modules 
+  const bedroomLightLeft 	= require("./bedroom/bedroomLeft.js"),
+    	bedroomLightRight 	= require("./bedroom/bedroomRight.js"),
+    	bedroomMotion 		= require("./bedroom/bedroomMotion.js"),
     		
-    		//Bathroom modules 
-    		bathroomLightServo 	= require("./bathroom/bathroomLight.js"),
-    		bathroomMotion 		= require("./bathroom/bathroomMotion.js");
-            console.log(bedroomLightLeft)
+    	//Bathroom modules 
+    	bathroomLightServo 	= require("./bathroom/bathroomLight.js"),
+    	bathroomMotion 		= require("./bathroom/bathroomMotion.js");
 })
 
 board.on("error", err => {event.emit("error", "boardSetUp: " + err)}); 
