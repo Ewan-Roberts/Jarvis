d59c799cc9389aba44815867929046cedfc1de06
Ewan <jojothegreat694@hotmail.com>
2018-04-22 21:26:14 +0100

    Update piServer.js

--- modules/piServer.js
+++ modules/piServer.js
@@ -1,25 +1,18 @@
 "use strict";
 
-const 	user 	= require("./userInformation"),
-		client  = require("socket.io-client")("http://192.168.1.100:" + user.port),
-		event 	= require("./event"),
-		storage = require("node-persist");
+const	user 	= require("./userInformation"),
+	client  = require("socket.io-client")("http://192.168.1.100:" + user.port),
+	event 	= require("./event"),
+	storage = require("node-persist");
 
 if(!client.connected) {event.emit("openApplication", "Raspberry")}
 
 // Set up the connection with the raspberry pi 
 event.on("corridorLight", bool => {client.emit("corridorLight", bool)})
 
-client.on("circleButton", () => {
-	console.log('circle');
-	event.emit("bedroomLightFlip");
+client.on("circleButton", () => {event.emit("bedroomLightFlip")}) 
 
-}) 
-
-client.on("squareButton", () => {
-
-	event.emit("bathroomLightFlip")
-})
+client.on("squareButton", () => {event.emit("bathroomLightFlip")})
 
 // Check if a connection with the pi module is set up, fire erorr if not
 event.once("checkPiStatus", () => {
