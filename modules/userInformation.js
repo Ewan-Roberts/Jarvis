7ff24c0e306dc0eaa1d92e91d2a24647d2dc0e13
Ewan <jojothegreat694@hotmail.com>
2018-05-08 21:17:42 +0100

    Update userInformation.js

--- modules/userInformation.js
+++ modules/userInformation.js
@@ -4,28 +4,28 @@
 module.exports = {
 
 	// for greating when user gets home
-	user_name: 				"Ewan",
-	override: 				false,
+	user_name: 			"",
+	override: 			false,
 
 	// for Spotify controls
-	spotify_user: 			"spotify:user:1130242707",
-	spotify_playlist: 		":playlist:6Gj9EYigkSxPRFox6rLSC8",
-	track: 					"spotify:track:1wYjs8KsIAhKB7yDjsAKmP",
+	spotify_user: 			"",
+	spotify_playlist: 		"",
+	track: 				"",
 
 	//Morning Settings
-	morning_track: 			"spotify:track:0y1QJc3SJVPKJ1OvFmFqe6",
-	morning_wait_till_music: 80000,
+	morning_track: 			"",
+	morning_wait_till_music: 	80000,
 	morning_alarm: 			"08:00",
 	
 	// which port local host will run on
-	port: 					3014,
+	port: 				3014,
 
 	// for API calls
-	weather_key: 			"b0b57b788f25abe95a358de9116a043d&units=metric&cnt=7",
-	news_key: 				"365037bafd7d9a2920f9e2fa8a522a8a:0:73403604",
+	weather_key: 			"",
+	news_key: 			"",
 
 	// when to start and stop turning on the lights 
-	daytime: 				["08:30","21:20"],
+	daytime: 			["08:30","21:20"],
 	reset_user: 			"17:00",
-	bedtime: 				"23:00"
+	bedtime: 			"23:00"
 }
