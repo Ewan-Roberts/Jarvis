05dd43cf7ce0e644c1f1a8f290b3c4a3a59ceefe
Ewan <jojothegreat694@hotmail.com>
2018-05-08 13:35:00 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,65 +1,87 @@
 "use strict";
 
+
+
 //External libraries
 const   childProc       = require("child_process"),
-        spotify         = require("spotify-node-applescript"),
         storage         = require("node-persist"),
+        event           = require("./modules/event"),
         
         //Service calls
         fetchNewsData   = require("./modules/services/fetchNewsData"),
         fetchWeatherData = require("./modules/services/fetchWeatherData"),
         wikiQuery       = require("./modules/services/wikiQuery"),
+        spanishQuizz    = require("./modules/services/spanishQuizz"),
+        spanish         = require("./modules/services/spanish"),
         
         //Internal libraries
         app             = require("./modules/speechServer"),
-        boardSetUp      = require("./modules/boardSetUp"),
-        digest          = require("./modules/digest"),
+        boardSetUp      = require("./modules/hardware/boardSetUp"),
         morning         = require("./modules/morning"),
         welcomeHome     = require("./modules/welcomeHome"),
-        event           = require("./modules/event"),
         timer           = require("./modules/timer"),
         piServer        = require("./modules/piServer"),
-        spanish         = require("./modules/spanish"),
         io              = require("socket.io").listen(server),
-        user            = require("./modules/userInformation"),
 
         //Over-ride kernal level functions
         musicControls   = require("./modules/kernal-overide/musicControls"),
         browserControls = require("./modules/kernal-overide/browserControls");
-        
+
 
 //Establish link with the front end and handle socket events
 io.on("connection", socket => { 
         
     storage.init()
 
+    // console.log(global.event)
+
     //Binds events to sockets: Sockets must be named after their event in order to stay tidy
-    event.eventNames().forEach(name => {socket.on(name, res => {event.emit(name,res)})})
+    global.event.eventNames().forEach(name => {socket.on(name, res => {global.event.emit(name,res)})})
 
     socket.on("bedroomLight", res=>{
         console.log(res)
-        event.emit("bedroomLight",res)
+        global.event.emit("bedroomLight",res)
+    })
+
+    socket.on("spanishQuizz", res=>{
+        
+        global.event.emit("spanishQuestion",res)
+    })
+
+    socket.on("bedroomRightToggle", res=>{
+        console.log("bedroomRightToggle " +res)
+        global.event.emit("bedroomRightToggle",res)
+    })
+    socket.on("bathroomLight", res=>{
+        console.log(res)
+        global.event.emit("bathroomLight",res)
     })
 
     socket.on("bathroomLight", res=>{
         console.log(res)
-        event.emit("bathroomLight",res)
+        global.event.emit("bathroomLight",res)
     })
 
     //Binds sockets to events: Events must be named after their socket in order to stay tidy
-    const socketList = ["speechFromBackEnd","wikiResult","news","speechFromBackEndSpanish","weather","spotifyTrackInfo","trackTimeInfo"]
+    const socket_list = ["speechFromBackEnd","wikiResult","news","speechFromBackEndSpanish","weather","spotifyTrackInfo","trackTimeInfo"]
 
-    socketList.forEach(name => {event.on(name, res => {socket.emit(name,res)})})
+    socket_list.forEach(name => {global.event.on(name, res => {socket.emit(name,res)})})
+
+    global.event.on("spanishWord", res => {
+
+        socket.emit("spanishWord",res)
+
+    })
 
 });
 
-event.once("checkStatus", () => {
+global.event.emit("checkStatus", () => {
     
-    let sessionData = io.nsps["/"].connected;
+    let session_data = io.nsps["/"].connected;
 
-    if (Object.keys(sessionData).length === 0) {
+    if (Object.keys(session_data).length === 0) {
     
-        event.emit("openApplication", "Jarvis") 
+        global.event.emit("openApplication", "Jarvis") 
     }
 });
 
