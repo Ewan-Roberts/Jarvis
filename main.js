3e219482f950ca2b638b0d6e28680c6eb05e0c30
Ewan <jojothegreat694@hotmail.com>
2018-05-14 13:58:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -38,40 +38,10 @@ io.on("connection", socket => {
     //Binds events to sockets: Sockets must be named after their event in order to stay tidy
     global.event.eventNames().forEach(name => {socket.on(name, res => {global.event.emit(name,res)})})
 
-    socket.on("bedroomLight", res=>{
-        console.log(res)
-        global.event.emit("bedroomLight",res)
-    })
-
-    socket.on("spanishQuizz", res=>{
-        
-        global.event.emit("spanishQuestion",res)
-    })
-
-    socket.on("bedroomRightToggle", res=>{
-        console.log("bedroomRightToggle " +res)
-        global.event.emit("bedroomRightToggle",res)
-    })
-    socket.on("bathroomLight", res=>{
-        console.log(res)
-        global.event.emit("bathroomLight",res)
-    })
-
-    socket.on("bathroomLight", res=>{
-        console.log(res)
-        global.event.emit("bathroomLight",res)
-    })
-
     //Binds sockets to events: Events must be named after their socket in order to stay tidy
     const socket_list = ["speechFromBackEnd","wikiResult","news","speechFromBackEndSpanish","weather","spotifyTrackInfo","trackTimeInfo"]
 
     socket_list.forEach(name => {global.event.on(name, res => {socket.emit(name,res)})})
-
-    global.event.on("spanishWord", res => {
-
-        socket.emit("spanishWord",res)
-
-    })
 
 });
 
@@ -86,22 +56,3 @@ global.event.emit("checkStatus", () => {
 });
 
 
-
-    // //Initiate local storage
-    // storage.init()
-
-    // const eventList = event.eventNames()
-
-    // //Binds events to sockets: Sockets must be named after their event in order to stay tidy
-    // eventList.forEach(name => {
-
-    //     console.log(name)
-        
-    //     socket.on(name, res => {event.emit(name,res)})
-
-    // })
-
-    // //Binds sockets to events: Events must be named after their socket in order to stay tidy
-    // const socketList = ["speechFromBackEnd","wikiResult","news","speechFromBackEndSpanish","weather","spotifyTrackInfo","trackTimeInfo"]
-
-    // socketList.forEach(name => {event.on(name, res => {socket.emit(name,res)})})
